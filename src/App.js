b3946acb59e1518ae7e59417bbb20d70913c602d
Tesfaye Hailu <tes.girmah@gmail.com>
2023-05-20 19:14:09 -0700

    Final Commity

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import React, { useState } from 'react';
+import Todo from './Components/ToDoList/index.js';
+import './App.css';
+
+
+const App = () => {
+  return (
+    <div className="App">
+          <Todo/>
+    </div>
+  );
+}
+
+export default App
+
